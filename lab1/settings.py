fd199e629928428e5f14a3bb7d7a764f4208495c
Kostek <ksytnik73kwer@gmail.com>
2024-05-31 18:09:56 +0200

    Refactor "settings.py" file

--- lab1/settings.py
+++ lab1/settings.py
@@ -1,16 +1,30 @@
 from utility.config import DbConfig, DjangoConfig
 import dj_database_url
 
-DB_URL = DbConfig().db_url()
-SECRET_KEY = DjangoConfig().retrieve_secret_key()
 
-# Postgre SQL
+# Load the configuration
+db_conf = DbConfig()
+dj_conf = DjangoConfig()
+
+# Get the needed variables for the configuration
+DB_URL = db_conf.db_url()
+SECRET_KEY = dj_conf.SECRET_KEY
+IS_DEBUG = dj_conf.DEBUG
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = SECRET_KEY
+
+# SECURITY WARNING: don't run the app with debug turned on in production!
+DEBUG = False
+
+# Database configuration
 DATABASES = {
     "default": dj_database_url.parse(
         url=DB_URL, conn_health_checks=True, conn_max_age=600
     )
 }
 
+# Application definition configuration
 INSTALLED_APPS = (
     "django.contrib.admin",
     "django.contrib.auth",
@@ -20,5 +34,3 @@ INSTALLED_APPS = (
     "django.contrib.staticfiles",
     "orm",
 )
-
-SECRET_KEY = SECRET_KEY
