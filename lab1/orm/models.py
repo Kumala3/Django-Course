9e49d776a3b11bdb81d78eea26f459f1885572b4
Kostek <ksytnik73kwer@gmail.com>
2024-05-31 22:54:13 +0200

    Add User model to ORM models, and add image to web-app service in "docker-compose.yml"

--- lab1/orm/models.py
+++ lab1/orm/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class User(models.Model):
+    user_id = models.BigAutoField(primary_key=True, null=False)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=8)
+    residence = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    dob = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    age = models.IntegerField()
+    is_active = models.BooleanField(default=True)
 
-
-# Define your first model from here:
+    def __str__(self):
+        return f"{self.first_name} has successfully created. Users's id: {self.user_id}"
