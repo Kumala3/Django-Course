f0791bca18b6f992623854d4601dfec4a9d21570
Kostek <ksytnik73kwer@gmail.com>
2024-06-01 17:56:18 +0200

    Add User model in crud app

--- lab2/crud/models.py
+++ lab2/crud/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-from django.utils.timezone import now
+from django.utils import timezone
 
-# Define your models from here:
 
+class User(models.Model):
+    username = models.CharField(max_length=100, null=False)
+    email = models.EmailField(max_length=254, null=False)
+    password = models.CharField(max_length=100, null=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.username} has been created with next email: {self.email}"
