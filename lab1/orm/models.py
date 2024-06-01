c29bb45328b3cbd4a43b1adff705fbd7ea33ba71
Kostek <ksytnik73kwer@gmail.com>
2024-06-01 08:22:39 +0200

    Update User model fields in "models.py"

--- lab1/orm/models.py
+++ lab1/orm/models.py
@@ -4,14 +4,14 @@ class User(models.Model):
     user_id = models.BigAutoField(primary_key=True, null=False)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=8)
-    residence = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    residence = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    age = models.IntegerField(null=False)
+    is_active = models.BooleanField(default=False)
     dob = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    age = models.IntegerField()
-    is_active = models.BooleanField(default=True)
-
+    
     def __str__(self):
         return f"{self.first_name} has successfully created. Users's id: {self.user_id}"
