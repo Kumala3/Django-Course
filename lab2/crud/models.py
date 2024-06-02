bf370b1f7b271b7017570600c2d685dc036dced8
Kostek <ksytnik73kwer@gmail.com>
2024-06-02 10:27:14 +0200

    Add Course, Lesson models and slightly refactored User model

--- lab2/crud/models.py
+++ lab2/crud/models.py
@@ -3,7 +3,8 @@ from django.utils import timezone
 
 
 class User(models.Model):
-    username = models.CharField(max_length=100, null=False)
+    firstname = models.CharField(max_length=100, null=False)
+    lastname = models.CharField(max_length=100, null=False)
     email = models.EmailField(max_length=254, null=False)
     password = models.CharField(max_length=100, null=False)
     created_at = models.DateTimeField(default=timezone.now)
@@ -14,8 +15,27 @@ class User(models.Model):
 
 
 class Instructor(User):
-    position = models.CharField(max_length=100, null=False)
-    department = models.CharField(max_length=100, null=False)
+    full_time = models.BooleanField(default=True)
+    total_learners = models.IntegerField()
 
     def __str__(self):
-        return f"{self.username} is an {self.position} in the {self.department} department"
+        return f"First name: {self.firstname}; \nLast name: {self.lastname}; \nIs full time: {self.full_time}; \nTotal learners: {self.total_learners}"
+
+
+class Course(models.Model):
+    name = models.CharField(null=False, max_length=100, default="online course")
+    description = models.CharField(null=False, max_length=500)
+
+    instructors = models.ManyToManyField(Instructor)
+
+    def __str__(self):
+        return f"Course name: {self.name}; Description: {self.description};"
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=200, default="title")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Lesson title: {self.title}; Course: {self.course};"
