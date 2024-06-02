705f5163e67ed85c5b97f5f8fad925d8eb88f3d6
Kostek <ksytnik73kwer@gmail.com>
2024-06-02 10:51:25 +0200

    Refactor "settings.py"

--- lab2/settings.py
+++ lab2/settings.py
@@ -1,17 +1,34 @@
-# PostgreSQL
+from utility.config import DbConfig, DjangoConfig
+import dj_database_url
+
+
+# Load the configuration
+db_conf = DbConfig()
+dj_conf = DjangoConfig()
+
+SECRET_KEY = dj_conf.SECRET_KEY
+db_url = db_conf.db_url()
+
+# Database configuration
+# https://docs.djangoproject.com/en/5.0/ref/databases/
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '#Replace it with generated password#',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.parse(
+        url=db_url, conn_max_age=600, conn_health_checks=True
+    )
 }
 
+# Installed apps definition
+# https://docs.djangoproject.com/en/5.0/ref/applications/
 INSTALLED_APPS = (
-    'crud',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "crud",
 )
 
-SECRET_KEY = 'SECRET KEY for this Django Project'
+# SECURITY WARNING: keep the secret key used in production secret!
+# https://docs.djangoproject.com/en/5.0/topics/security/
+SECRET_KEY = SECRET_KEY
