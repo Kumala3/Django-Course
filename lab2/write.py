cc7d65f4026d68bc1425b22f6cd2c13ac6461c12
Kostek <ksytnik73kwer@gmail.com>
2024-06-02 11:46:12 +0200

    feat: Implement write _instructors function and refactor imports

--- lab2/write.py
+++ lab2/write.py
@@ -1,17 +1,49 @@
 # Django specific settings
-import inspect
 import os
+from crud.models import User, Learner, Course, Enrollment, Instructor, Lesson
+from datetime import date
+from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-from django.db import connection
-
-# Ensure settings are read
-from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
 
-from crud.models import *
-from datetime import date
 
+def write_instructors():
+    # Add instructors
+    # Create a user
+    user_john = User(first_name="John", last_name="Doe", dob=date(1962, 7, 16))
+    user_john.save()
+    instructor_john = Instructor(full_time=True, total_learners=30050)
+    # Update the user reference of instructor_john to be user_john
+    instructor_john.user = user_john
+    instructor_john.save()
 
-# Your code starts from here:
+    instructor_yan = Instructor(
+        first_name="Yan",
+        last_name="Luo",
+        dob=date(1962, 7, 16),
+        full_time=True,
+        total_learners=30050,
+    )
+    instructor_yan.save()
+
+    instructor_joy = Instructor(
+        first_name="Joy",
+        last_name="Li",
+        dob=date(1992, 1, 2),
+        full_time=False,
+        total_learners=10040,
+    )
+    instructor_joy.save()
+
+    instructor_peter = Instructor(
+        first_name="Peter",
+        last_name="Chen",
+        dob=date(1982, 5, 2),
+        full_time=True,
+        total_learners=2002,
+    )
+    instructor_peter.save()
+    print("Instructor objects all saved... ")
