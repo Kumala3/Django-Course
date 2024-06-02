c741b5676b5d7fe0119df3c0d43900606ffda130
Kostek <ksytnik73kwer@gmail.com>
2024-06-02 08:49:52 +0200

    Remove "auto_now_add" option from "dob" field in User model

--- lab1/orm/models.py
+++ lab1/orm/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class User(models.Model):
     user_id = models.BigAutoField(primary_key=True, null=False)
     first_name = models.CharField(max_length=100)
@@ -9,9 +10,9 @@ class User(models.Model):
     email = models.EmailField(max_length=254)
     age = models.IntegerField(null=False)
     is_active = models.BooleanField(default=False)
-    dob = models.DateTimeField(auto_now_add=True)
+    dob = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return f"{self.first_name} has successfully created. Users's id: {self.user_id}"
