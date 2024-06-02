9f2b86c5f5c7faf0d519e7e935bb46327c12a415
Kostek <ksytnik73kwer@gmail.com>
2024-06-02 14:42:18 +0200

    Refactor "admin.py" to display specific fields for each model in Django admin

--- lab2/crud/admin.py
+++ lab2/crud/admin.py
@@ -5,29 +5,29 @@ from .models import User, Course, Instructor, Learner, Enrollment, Lesson
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["__all__"]
+    list_display = ["firstname", "lastname", "dob", "created_at"]
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ["__all__"]
+    list_display = ["name", "description"]
 
 
 @admin.register(Instructor)
 class InstructorAdmin(admin.ModelAdmin):
-    list_display = ["__all__"]
+    list_display = ["firstname", "lastname", "dob", "full_time", "total_learners"]
 
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ["__all__"]
+    list_display = ["course", "content", "title"]
 
 
 @admin.register(Learner)
 class LearnerAdmin(admin.ModelAdmin):
-    list_display = ["__all__"]
+    list_display = ["firstname", "lastname", "dob", "social_link", "occupation"]
 
 
 @admin.register(Enrollment)
 class EnrollmentAdmin(admin.ModelAdmin):
-    list_display = ["__all__"]
+    list_display = ["date_enrolled", "course", "learner", "mode"]
