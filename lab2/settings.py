b5c0fbbcad49ce8f7935ba83a3423432367eb9dd
Kostek <ksytnik73kwer@gmail.com>
2024-06-02 11:30:12 +0200

    feat: Add static file configuration in settings.py

--- lab2/settings.py
+++ lab2/settings.py
@@ -71,6 +71,10 @@ TEMPLATES = [
     },
 ]
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = "static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
