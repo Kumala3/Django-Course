b743ac6543a844134656fb0a3a17b5b727413db7
Kostek <ksytnik73kwer@gmail.com>
2024-06-02 10:33:44 +0200

    Refactor User model to include dob field and add Learner model

--- lab2/crud/models.py
+++ lab2/crud/models.py
@@ -7,6 +7,7 @@ class User(models.Model):
     lastname = models.CharField(max_length=100, null=False)
     email = models.EmailField(max_length=254, null=False)
     password = models.CharField(max_length=100, null=False)
+    dob = models.DateField(null=False)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
 
@@ -20,6 +21,28 @@ class Instructor(User):
 
     def __str__(self):
         return f"First name: {self.firstname}; \nLast name: {self.lastname}; \nIs full time: {self.full_time}; \nTotal learners: {self.total_learners}"
+
+
+class Learner(User):
+    STUDENT = "student"
+    DEVELOPER = "developer"
+    DATA_SCIENTIST = "data_scientist"
+    DATABASE_ADMIN = "dba"
+
+    OCCUPATION_CHOICES = [
+        (STUDENT, "Student"),
+        (DEVELOPER, "Developer"),
+        (DATA_SCIENTIST, "Data Scientist"),
+        (DATABASE_ADMIN, "Database Admin"),
+    ]
+
+    occupation = models.CharField(
+        null=False, max_length=20, choices=OCCUPATION_CHOICES, default=STUDENT
+    )
+    social_link = models.URLField(max_length=200)
+
+    def __str__(self):
+        return f"First name: {self.first_name}\nLast name: {self.last_name}\nDob: {self.dob}\nOccupation: {self.occupation}\nSocial link: {self.social_link}"
 
 
 class Course(models.Model):
