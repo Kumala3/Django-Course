bdc4c6670b9a92ad80e6cbc46725d44f9a0242d6
Kostek <ksytnik73kwer@gmail.com>
2024-06-08 21:46:33 +0200

    Refactor admin.py for lab4 adminsite app

--- lab4/adminsite/admin.py
+++ lab4/adminsite/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Course, Instructor, Lesson
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "total_enrollment", "description", "pub_date")
+
+
+@admin.register(Instructor)
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ("full_time", "total_learners")
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("title", "order", "course")
