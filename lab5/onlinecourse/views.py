6828e986598ff141dbccdbb329a91079e8315e80
Kostek <ksytnik73kwer@gmail.com>
2024-06-08 22:24:13 +0200

    Add initial files for lab5 views-and-templates app

---
+++ lab5/onlinecourse/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Course, Lesson, Enrollment
+from django.urls import reverse
+from django.views import generic
+from django.http import Http404
+
+# Create your views here.
+
