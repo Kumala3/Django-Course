f432eb5921c66fac0e113c995c607f88b6def3e6
Kostek <ksytnik73kwer@gmail.com>
2024-06-09 09:02:05 +0200

    feat: Add EnrollCourse and CourseDetails views and include them to URLs

--- lab5/onlinecourse/urls.py
+++ lab5/onlinecourse/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import TopCoursesList
+from .views import TopCoursesList, EnrollCourse, CourseDetails
 
 app_name = "onlinecourse"
 
 urlpatterns = (
     [
-        path("top-courses/", TopCoursesList.as_view(), name="popular_course_list")
+        path("top-courses/", TopCoursesList.as_view(), name="popular_course_list"),
+        path("course/<int:course_id>/enroll/", EnrollCourse.as_view(), name="enroll"),
+        path("course/<int:course_id>/", CourseDetails.as_view(), name="course_details"),
     ]
 )
--- lab5/onlinecourse/views.py
+++ lab5/onlinecourse/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Course, Lesson, Enrollment
 from django.urls import reverse
@@ -9,8 +9,28 @@ from django.views import View
 
 
 class TopCoursesList(View):
-    def get(self, request):
+    def get(self, request: HttpRequest):
         context = {}
         courses = Course.objects.order_by("-total_enrollment")[:10]
         context["course_list"] = courses
         return render(request, "onlinecourse/course_list.html", context)
+
+
+class EnrollCourse(View):
+    def post(self, request: HttpRequest, course_id: int):
+        course = get_object_or_404(Course, pk=course_id)
+        course.total_enrollment += 1
+        course.save()
+        return HttpResponseRedirect(
+            reverse(viewname="onlinecourse:course_details", args=(course_id,))
+        )
+
+
+class CourseDetails(View):
+    def get(self, request: HttpRequest, course_id: int):
+        try:
+            course = Course.objects.get(pk=course_id)
+            context = {"course": course}
+            return render(request, "onlinecourse/course_detail.html", context)
+        except Course.DoesNotExist:
+            raise Http404("Course does not exist")
